c1d144775a6f34c84adc94338fe2a96a104dd09f
Manuj Rai <imanujrai7@gmail.com>
2024-12-04 12:20:31 +0530

    modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @Component({
   selector: 'app-root',
-  standalone:true,
-  imports:[NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
+
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     ContactComponent
   ], 
   imports: [
