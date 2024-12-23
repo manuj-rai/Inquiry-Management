3bbaed7d489609714a33d21e5c9cfb9986972072
Manuj Rai <imanujrai7@gmail.com>
2024-12-23 11:28:37 +0530

    modified:   src/app/main-layout/home/home.component.html 	modified:   src/app/main-layout/home/home.component.ts

--- src/app/main-layout/home/home.component.ts
+++ src/app/main-layout/home/home.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { TsParticlesComponent } from "../../Reusables/TsParticles";
+import { AlertService } from '../../services/alert.service';
+AlertService
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TsParticlesComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  constructor(
+    private alertServic : AlertService
+  ){}
+
+  alert() {
+  this.alertServic.warning("Currently unavailable. Please check back later.")
+  }
 }
